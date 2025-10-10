b4fc6cd5ac8895ff78c645b5d19c4d1f3468f80f
shruti <shrutisrivastava2019@gmail.com>
2025-10-11 01:09:50 +0530

    Use .env variables for database and secret key

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ load_dotenv()  # this will load environment variables from .env
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASS')
+db_host = os.environ.get('DB_HOST')
+db_name = os.environ.get('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
